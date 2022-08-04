26418d79b2d9da6c6deb7d7a431f7a213a9c2468
Minh Bui <minhbibui@gmail.com>
2022-08-04 16:27:03 -0400

    containsDuplicate problem

---
+++ java-main-class/src/array/containsDuplicate.java
@@ -0,0 +1,52 @@
+package array;
+import java.util.Arrays;
+/*
+* Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+* */
+public class containsDuplicate {
+    public boolean containsDuplicated(int[] nums){
+        // linear approach
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i]  == nums[i+1]){
+                return true;
+            }
+        }
+        // N square approach
+       /*
+       for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(i != j){
+                    if(nums[i] == nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        */
+        return false;
+    }
+}
