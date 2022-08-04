5c196cc5d729508a96ebdea7195bc9dc3a5d89d7
Minh Bui <minhbibui@gmail.com>
2022-08-04 15:35:53 -0400

    solved problem 2

--- java-main-class/src/run.java
+++ java-main-class/src/run.java
@@ -1,6 +1,17 @@
+import array.bestTimeToBuyAndSellStock;
 public class run {
     public static void main(String[] args)
     {
+        // ---------------------------------------------------------------------------
+        // Test best time to buy stock function
+        bestTimeToBuyAndSellStock bestTime = new  bestTimeToBuyAndSellStock();
+        int[] stockPrice = new int[5];
+        stockPrice[0] = 4;
+        stockPrice[1] = 2;
+        stockPrice[2] = 5;
+        stockPrice[3] = 3;
+        stockPrice[4] = 7;
+        System.out.println(bestTime.maxProfit(stockPrice));
 
     }
 }
